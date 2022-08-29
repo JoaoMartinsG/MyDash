2e077c69c56ef647664c57b915e2c3f73f56df4b
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-08-29 21:24:27 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_191740) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_201155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "maintenances", force: :cascade do |t|
+    t.bigint "vehicle_id", null: false
+    t.string "title"
+    t.string "description"
+    t.string "priority"
+    t.integer "recorrency"
+    t.float "price"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["vehicle_id"], name: "index_maintenances_on_vehicle_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +41,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_191740) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vehicles", force: :cascade do |t|
+    t.string "type_of_vehicle"
+    t.string "make"
+    t.string "model"
+    t.string "plate"
+    t.integer "year"
+    t.integer "mileage"
+    t.string "driver"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
+  end
+
+  add_foreign_key "maintenances", "vehicles"
+  add_foreign_key "vehicles", "users"
 end
