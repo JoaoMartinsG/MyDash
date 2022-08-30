b31390db0dbac8c03187c17d992d9e392527dc6b
Lochlan Savage <lochlansavage@live.ca>
2022-08-30 23:13:15 +0100

    added icons to icon folder in Assets/images

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  root to: "pages#home"
+  get '/dashboard', to: 'pages#dashboard'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :vehicles, only: %i[new  create show destroy index] do
+    resources :bookings, only: %i[new create] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
