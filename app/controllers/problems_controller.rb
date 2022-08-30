3523ed8880a62b811aaa8f44701491f8a6f6697c
joao martins <joaomgwork@hotmail.com>
2022-08-30 15:04:51 +0100

    changed a bit o problems controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,2 +1,36 @@
 class ProblemsController < ApplicationController
+  def index
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @problems = @vehicle.problems
+    end
+  end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+
+    if @vehicle.save
+      redirect_to vehicle_path(@vehicle)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    @vehicle.update(vehicle_params)
+    redirect_to vehicle_path(@vehicle)
+  end
 end
