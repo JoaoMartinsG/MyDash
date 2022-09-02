6f3241f72a3449d7c99da82a351f90e393d98909
joao martins <joaomgwork@hotmail.com>
2022-09-02 11:04:37 +0100

    added overview route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :overview, only: [:index]
   resources :vehicles do
     resources :maintenances
     resources :problems
