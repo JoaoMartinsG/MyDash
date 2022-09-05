c4f7e39063a625149c541cf49ab3d8db3f850ec1
joao martins <joaomgwork@hotmail.com>
2022-09-05 16:28:13 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :overview, only: [:index]
   patch "/vehicles/:vehicle_id/maintenances/:id/done", to: "maintenances#done", as: :done_vehicle_maintenance
+  patch "/vehicles/:vehicle_id/problems/:id/done", to: "problems#done", as: :done_vehicle_problem
+  patch "/vehicles/:vehicle_id/wishlists/:id/done", to: "wishlists#done", as: :done_vehicle_wishlist
+
   resources :vehicles do
     resources :maintenances
     resources :problems
