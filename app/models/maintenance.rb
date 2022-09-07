6e302611e87934e24fb7cb0092625e0e55a98a46
joao martins <joaomgwork@hotmail.com>
2022-09-07 12:59:44 +0100

    did all the validations

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -1,4 +1,5 @@
 class Maintenance < ApplicationRecord
   belongs_to :vehicle
+  validates :title, :priority, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,5 @@
 class Problem < ApplicationRecord
   belongs_to :vehicle
+  validates :title, :priority, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
