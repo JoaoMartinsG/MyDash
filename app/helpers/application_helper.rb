286f0dc879d5cfc92308ac576f4af2cbff44ffb1
joao martins <joaomgwork@hotmail.com>
2022-09-08 14:35:14 +0100

    fixed bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bike_image(brand)
+    formatted_brand = brand.downcase.gsub(' ', '')
+    brands = %w[ducati benelli harleydavidson kawasaki keeway norton suzuki yamaha]
+    if brands.include?(formatted_brand)
+      "brands/#{brand.downcase}.png"
+    else
+      'Icons/motorbike_icon.png'
+    end
+  end
 end
